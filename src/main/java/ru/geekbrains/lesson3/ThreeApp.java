1539fa7c0f480ac8162bc6b62ef0886677ac918a
Daniil <denko@list.ru>
2020-03-23 17:06:51 +0500

    Здравствуйте. Как сделать чтобы цикл повторной игры запускался от строкового типа переменной? В данной реализации не совсем это обязательно, но интересно. Как реализовать счетчик использованных попыток?

---
+++ src/main/java/ru/geekbrains/lesson3/ThreeApp.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.lesson3;
+
+import java.util.Scanner;
+
+public class ThreeApp {
+    public static void main(String[] args) {
+        while (true) {
+            FunctionNumber();
+            int ExitUserNumber = 0;
+            int ReturnUserNumber = 1;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Хотите повторить?");
+            int EnterUserNumber = scanner.nextInt();
+                if (EnterUserNumber == ExitUserNumber) {
+                    break;
+                } else if (EnterUserNumber == ReturnUserNumber) {
+                    continue;
+
+                }
+        }
+    }
+
+
+
+    public static void FunctionNumber () {
+         int number = (int) (Math.random() * 10);
+         int chance = 0;
+            while (chance < 3) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Введите число от 0 до 9");
+                int userNumber = scanner.nextInt();
+                if (number == userNumber) {
+                    System.out.println("Правильно");
+                    System.out.println("Игра окончена");
+                    break;
+                } else if (number > userNumber) {
+                    System.out.println("Загаданное число больше" + " " + userNumber);
+                    chance++;
+                } else if (number < userNumber) {
+                    System.out.println("Загаданное число меньше" + " " + userNumber);
+                    chance++;
+                }
+            }
+
+        }
+    }
+
